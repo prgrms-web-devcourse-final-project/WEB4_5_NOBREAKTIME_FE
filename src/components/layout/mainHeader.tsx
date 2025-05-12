6513275c9e7b824495507b20ddb9de98775568fa
sik2 <bickwang123@gmail.com>
2025-05-12 14:26:57 +0900

    fix: header check login

--- src/components/layout/mainHeader.tsx
+++ src/components/layout/mainHeader.tsx
@@ -1,11 +1,21 @@
 'use client'
 
+import { useGlobalLoginMember } from '@/stores/auth/loginMember'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 
 export default function MainHeader() {
     const router = useRouter()
+    const { isLogin } = useGlobalLoginMember()
+
+    const handleButtonClick = () => {
+        if (isLogin) {
+            router.push('/dashboard')
+        } else {
+            router.push('/login')
+        }
+    }
 
     return (
         <header className="flex justify-between items-center px-10 py-5">
@@ -14,9 +24,9 @@ export default function MainHeader() {
             </Link>
             <button
                 className="bg-[var(--color-sub-1)] rounded-sm text-[var(--color-main)] text-md px-4 py-2 font-bold"
-                onClick={() => router.push('/login')}
+                onClick={handleButtonClick}
             >
-                Login
+                {isLogin ? 'Go to Dashboard' : 'Login'}
             </button>
         </header>
     )
